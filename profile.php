80c8b587c53c289865de2154094f4d0a9e8e3d6e
Gasar Iyali <gasariyali@gmail.com>
2021-05-04 07:37:25 +0100

    profile page with initial post area

--- profile.php
+++ profile.php
@@ -5,16 +5,17 @@
     <title>Profile | Nollywood Kannywood</title>
     <link rel="stylesheet" href="style/style.css">
   </head>
-
-  <body class="container">
-    <br>
+  <body>
     <!-- Top Bar Blue color -->
-    <div class="profile_bar">
-        <div class="profile_header">
- Majalisan Nollywood Kannywood &nbsp &nbsp
- <input type="text" id="search_box" name="" value="" placeholder="Search for actors, musicians, film makers, other people and friends...">
-</div>  <img src="img/selfie.jpg" style="width: 75px;float: right;padding-bottom: 30px;margin-right:250px;margin-top: -63px;">
-</div> <!-- End of blue bar -->
+    <div class="container">
+  <div class="profile_bar">
+    Majalisa Nollywood Kannywood
+</div>
+</div>
+
+</div>
+
+<!-- End of blue bar -->
 <div class="wrapper">
   <div class="box">
     <img src="img/mountain.jpg" class="image" style="width: 100%"alt="">
@@ -28,25 +29,33 @@
     <div class="menu_buttons">Photos</div>
     <div class="menu_buttons">Settings</div>
      <br><br>
-  </d0iv>
-  <!-- below the cover area-->
-  <div class="d-flex">
+  </div>
 
+  <!-- below the cover area-->
+
+  <div class="d-flex">
       <!-- left aside is friends area-->
     <div class="left_aside">
-<p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
-Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC</p>
+      <div class="friends">
+        Friends
+      </div>
 <div class="friends_bar">
-  <div class="friends">
-    Friends
-  </div>
+
   <div>
   <p> <img class="friends_img" src="img/DSC_0027.jpg">
     Zack and Nafisat</p><br><br>
 </div>
 <div>
+  <p> <img class="friends_img" src="img/samira.png">
+    Samira Modibbo</p><br><br>
+</div>
+<div>
   <p><img class="friends_img" src="img/selfie.jpg">
   Mary Banda</p><br><br>
+</div>
+<div>
+  <p><img class="friends_img" src="img/aisha.jpg">
+  Aisha Salisu Babangida</p><br><br>
 </div>
 <div>
   <p><img class="friends_img" src="img/user3.jpg">
@@ -65,8 +74,37 @@ Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC</
         <textarea name="name" rows="8" cols="80" placeholder="Make your comment here and say what is on your mind?
          Or give a shout out to your friends!"></textarea><br>
          <input id="post_button" type="submit" name="submit" value="Post">
+         <br>
+      </div>
+      <!-- posts  -->
+      <div class="post_bar">
+        <div class="post">
+          <div class="friends_img"> <img src="img/samira.png" style="width: 80px;"alt=""> </div>
+          <div class="user_post">"I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."<br></div>
+          <div class="like">
+            <a href="#">Likes</a> <a href="#">comments</a> May 5, 2021
+          </div>
+      </div><br>
+
+      <div class="post">
+        <div class="friends_img"> <img src="img/aisha.jpg" style="width: 80px; padding: 2px;margin-right: 10px;"alt=""> </div>
+        <div class="user_post"><br><br>"To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</div>
+        <div class="like">
+          <a href="#">Likes</a>&nbsp  <a href="#">comments</a> May 5, 2021
+        </div>
+    </div><br>
+    <div class="post">
+      <div class="friends_img"> <img src="img/zack.jpg" style="width: 80px;"alt=""> </div>
+      <div class="user_post"><br><br>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</div>
+      <div class="like" style="width 50px; margin-bottom: 0px; float: right;">
+        <a href="#">Likes</a> &nbsp <a href="#">comments</a> &nbsp May 5, 2021<br>
+  </div>
+
+  </div>
+
       </div>
 </div>
 </div>
+  </div>
   </body>
 </html>
